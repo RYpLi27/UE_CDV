5bbf6e6faf334d605f0ddc91950d51b67cfda6f2
JakubPowierski <kubapowierski1003199522@gmail.com>
2026-02-10 03:05:33 +0100

    xd

---
+++ Source/UE_CDV/Private/MyActoreqwe.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyActoreqwe.h"
+
+// Sets default values
+AMyActoreqwe::AMyActoreqwe()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMyActoreqwe::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyActoreqwe::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/UE_CDV/Private/MyActorwqe.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyActorwqe.h"
+
+// Sets default values
+AMyActorwqe::AMyActorwqe()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMyActorwqe::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyActorwqe::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
